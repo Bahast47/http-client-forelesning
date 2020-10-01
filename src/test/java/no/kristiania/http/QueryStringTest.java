db1ba8530b08b9e74364ef575d6c3e234c7543c7
Bahast47 <69916673+Bahast47@users.noreply.github.com>
2020-10-01 02:52:54 +0200

    added test for multiple parameter (failling)

--- src/test/java/no/kristiania/http/QueryStringTest.java
+++ src/test/java/no/kristiania/http/QueryStringTest.java
@@ -1,5 +1,6 @@
 package no.kristiania.http;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,5 +11,26 @@ public class QueryStringTest {
     void shouldRetriveSatusCode(){
         QueryString queryString = new QueryString("status=200");
         assertEquals("200", queryString.getParameter("status"));
+    }
+
+    @Test
+    void shouldRetrieveStatusCode_401(){
+        QueryString queryString = new QueryString("statuse-404");
+        assertEquals("401", queryString.getParameter("status"));
+    }
+
+    @Test
+    void shouldReturnNullForMissingParameters(){
+        QueryString queryString = new QueryString("body-Hello");
+        Assertions.assertNull(queryString.getParameter("status"));
+
+    }
+
+    @Test
+    void shouldSupportMultipleParameters(){
+        QueryString queryString = new QueryString("status= 200&body=Hello");
+        assertEquals("200", queryString.getParameter("status"));
+        assertEquals("Hello", queryString.getParameter("body"));
+
     }
 }
