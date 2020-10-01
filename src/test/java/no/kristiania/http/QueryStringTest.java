3a98fb0d351f533963ec6d88f193fbabfef849bf
Bahast47 <69916673+Bahast47@users.noreply.github.com>
2020-10-01 03:48:28 +0200

    Fix maven tests

--- src/test/java/no/kristiania/http/QueryStringTest.java
+++ src/test/java/no/kristiania/http/QueryStringTest.java
@@ -9,27 +9,28 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 public class QueryStringTest {
 
     @Test
-    void shouldRetriveSatusCode(){
+    void shouldRetrieveStatusCode(){
         QueryString queryString = new QueryString("status=200");
         assertEquals("200", queryString.getParameter("status"));
     }
 
     @Test
     void shouldRetrieveStatusCode_401(){
-        QueryString queryString = new QueryString("statuse-404");
+        QueryString queryString = new QueryString("status=401");
         assertEquals("401", queryString.getParameter("status"));
     }
 
     @Test
     void shouldReturnNullForMissingParameters(){
-        QueryString queryString = new QueryString("body-Hello");
+        QueryString queryString = new QueryString("body=Hello");
         assertNull(queryString.getParameter("status"));
 
     }
 
     @Test
     void shouldSupportMultipleParameters(){
-        QueryString queryString = new QueryString("status= 200&body=Hello");
+        QueryString queryString = new QueryString("status=200&body=Hello");
+
         assertEquals("200", queryString.getParameter("status"));
         assertEquals("Hello", queryString.getParameter("body"));
 
